a44c714b284eab8799a73765c082a72106261eae
julydeath <manojkarajada.mk@gmail.com>
2023-09-19 01:13:29 +0530

    chore : basic quiz app

---
+++ components/Quiz.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { CssBaseline, Box, Container } from "@mui/material";
+import { lightBlue } from "@mui/material/colors";
+
+import QuestionCard from "./QuestionCard";
+import Result from "./Result";
+import questions from "../public/data/list";
+
+function Quiz() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const finishedQuiz = currentQuestionIndex === questions.length;
+  const currentQuestion = questions[currentQuestionIndex];
+
+  const goToNext = () => {
+    setCurrentQuestionIndex((prevState) => prevState + 1);
+  };
+
+  const submitAnswer = (value) => {
+    setAnswers((prevState) => [...prevState, value]);
+    goToNext();
+  };
+
+  const restartQuiz = () => {
+    setCurrentQuestionIndex(0);
+    setAnswers([]);
+  };
+
+  return (
+    <div>
+      <CssBaseline />
+      <Box
+        sx={{
+          backgroundColor: lightBlue[500],
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Container maxWidth="sm">
+          {finishedQuiz ? (
+            <Result restartQuiz={restartQuiz} answers={answers} />
+          ) : (
+            <QuestionCard
+              question={currentQuestion}
+              questionNumber={currentQuestionIndex + 1}
+              submitAnswer={submitAnswer}
+            />
+          )}
+        </Container>
+      </Box>
+    </div>
+  );
+}
+
+export default Quiz;
